9e5dd49846260d7f52614b4241afc29fd56088ef
Jack Roscoe <jack.x.roscoe@gmail.com>
2025-03-08 00:07:01 -0500

    added some mor packets

--- teams/binhampton/rerun_explot.sh
+++ teams/binhampton/rerun_explot.sh
@@ -4,7 +4,7 @@ while [ true ]
 do
   # ports 3-3 for my laptop
   echo 'Cycling board power...'
-  uhubctl -l 3-3 -a cycle
+  uhubctl -a cycle
   sleep 10
   echo 'Running decryption oracles...'
   python packet_collision.py
