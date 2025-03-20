f99d59553b6cfea406e54076a2d8e3b5c8e69815
Jack Roscoe <jack.x.roscoe@gmail.com>
2025-03-19 20:10:19 -0400

    removed .venv and egg-info from git

---
+++ scripts/setup-python-venv.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cd $(dirname $0)
+
+python -m venv .venv --prompt ectf-attack
+. ./.venv/bin/activate
+
+# misc dependancies
+python -m pip install pwntools loguru
+
+# host tools from organizers
+python -m pip install ectf_host_tools/
+
+# attack scripts and utilities
+python -m pip install -e ectf25_attack/
